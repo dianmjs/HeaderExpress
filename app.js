ec9751e5dabd27a41a2df8440da36462f8e40d0a
dianmjs <dianamjs69@gmail.com>
2020-06-10 14:11:48 -0500

    header1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  var url = req.get("Content-Type");
+  var url = req.get("User-Agent");
   res.send("<h1>" + url + "</h1>");
 });
 
